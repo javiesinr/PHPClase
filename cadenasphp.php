d6c3a9f92faec54986670a8fbdc6175b6d88ff69
javiesinr <82998767+javiesinr@users.noreply.github.com>
2021-12-18 21:58:10 +0100

    Update cadenasphp.php

--- cadenasphp.php
+++ cadenasphp.php
@@ -4,3 +4,17 @@ $text2=$_GET["operador2"];
 $text3=$_GET["operador3"];
 $selector=$_GET["cadenas"];
 //creacion de variables.
+function Cuenta_caractere($text1){
+
+    $res=strlen($text1);
+
+    print "El resultado es ".$res."<br>";
+
+}
+
+function Cuentas_palabras($text1){
+    
+    $res=str_word_count($text1);
+
+    print "el resultado es ".$res."<br>";
+}
